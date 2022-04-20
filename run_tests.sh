23f73938bafc74b55ce918eb4415dfced7784363
Pascal Pieper <accounts@pascalpieper.de>
2022-04-20 18:33:01 +0200

    Fastmode (same functionality as old mode) now only 3 percent slower than vanilla

--- run_tests.sh
+++ run_tests.sh
@@ -13,10 +13,16 @@ killall vp-breadboard
 
 echo "Doing new protocol disconnected test..."
 { time hifive-vp --disable-inline-oled spi_oled/spi_oled ; } &> test_run_NP_disco.txt
-vp-breadboard -c :/conf/oled_iof.json &
-echo "Doing new protocol connected test..."
+vp-breadboard -c :/conf/oled_iof_bidirectional.json &
+echo "Doing new protocol connected test, bidirectional..."
 sleep 1
-{ time hifive-vp --disable-inline-oled --wait-for-gpio spi_oled/spi_oled ; } &> test_run_NP_connected.txt
+{ time hifive-vp --disable-inline-oled --wait-for-gpio spi_oled/spi_oled ; } &> test_run_NP_connected_bidirectional.txt
+killall vp-breadboard
+
+vp-breadboard -c :/conf/oled_iof_noresponse.json &
+echo "Doing new protocol connected test, one-way spi (noresponse mode)..."
+sleep 1
+{ time hifive-vp --disable-inline-oled --wait-for-gpio spi_oled/spi_oled ; } &> test_run_NP_connected_noresponse.txt
 killall vp-breadboard
 
 
